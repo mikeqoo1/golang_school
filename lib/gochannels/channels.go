735ffae9fcb0fc105e250b29f827022c50dc6c77
seaflower <mikeqoo123@gmail.com>
2018-11-16 10:46:32 +0800

    更新channel

--- lib/gochannels/channels.go
+++ lib/gochannels/channels.go
@@ -16,13 +16,20 @@ func TestChannel() {
 	go blackpink("A", "B", "C", "DXYZP")
 	go blackpink("P", "I", "N", "KNBAA")
 	go blackpink("0", "0", "0", "0")
+	b2 := []byte{0, 130, 0, 0, 0, 1, 91, 238, 40, 232, 3, 79, 102, 1, 4, 2, 88, 0, 48, 1, 4, 1, 4,
+		66, 69, 48, 48, 49, 0, 0, 0, 1, 66, 48, 48, 48, 48, 49, 50, 49, 2, 84, 88, 70, 67, 57, 32,
+		32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 0, 16, 5, 144, 0, 1, 0, 132, 38,
+		102, 49, 1, 2, 0, 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 91, 238, 40, 232, 3, 79, 91, 238, 40, 232, 3, 79, 4, 0, 0,
+		24, 81, 0, 0, 0, 1, 1, 44}
+	go testpush(b2)
 	time.Sleep(time.Microsecond * 100)
 	time.Sleep(time.Microsecond * 100)
 	go bigbang()
 	go bigbang()
 	go bigbang()
-	//go bigbang()
-	//go bigbang()
+	go bigbang()
+	go bigbang()
 	time.Sleep(time.Microsecond * 100)
 	time.Sleep(time.Microsecond * 100)
 }
@@ -38,7 +45,7 @@ func testpush(A []byte) {
 
 func bigbang() {
 	B := testpop()
-	fmt.Println(B)
+	fmt.Println(string(B))
 }
 
 func testpop() []byte {
