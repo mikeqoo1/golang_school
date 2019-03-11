c429fed7557e0c90cbeb9278487814d9dc4e6648
seaflower <mikeqoo123@gmail.com>
2019-03-11 09:41:24 +0800

    整合leetCode的解題方法(#17)

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	D "golang_school/lib/db"
 	C "golang_school/lib/gochannels"
+	L "golang_school/lib/leetcode"
 	B "golang_school/lib/string"
 	A "golang_school/lib/time"
 )
@@ -31,38 +32,7 @@ func main() {
 	C.TestChannel()
 	nums1 := []int{1, 2}
 	nums2 := []int{3, 4}
-	Q := findMedianSortedArrays(nums1, nums2)
+	Q := L.FindMedianSortedArrays(nums1, nums2)
 	fmt.Println(Q)
 
-}
-
-//findMedianSortedArrays 尋找中位數
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	i, j := 0, 0
-	nums3 := []int{}
-	for i < len(nums1) && j < len(nums2) {
-		if nums1[i] < nums2[j] {
-			nums3 = append(nums3, nums1[i])
-			i = i + 1
-		} else {
-			nums3 = append(nums3, nums2[j])
-			j = j + 1
-		}
-	}
-	if i < len(nums1) {
-		for ; i < len(nums1); i++ {
-			nums3 = append(nums3, nums1[i])
-		}
-	}
-	if j < len(nums2) {
-		for ; j < len(nums2); j++ {
-			nums3 = append(nums3, nums2[j])
-		}
-	}
-	lenSums3 := len(nums3)
-	if lenSums3%2 == 0 {
-		return (float64(nums3[lenSums3/2-1]) + float64(nums3[lenSums3/2])) / 2.0
-	} else {
-		return float64(nums3[(lenSums3+1)/2-1])
-	}
 }
