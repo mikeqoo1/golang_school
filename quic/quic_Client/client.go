12c2dc3de9bc3fb07447d20813b737f6aee21ce1
seaflower <mikeqoo123@gmail.com>
2023-08-07 15:40:38 +0800

    基本QUIC範例

---
+++ quic/quic_Client/client.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+
+	"crypto/tls"
+
+	"fmt"
+	"io"
+
+
+	"github.com/quic-go/quic-go"
+)
+
+const addr = "localhost:4242"
+
+const message = "Hi I am Client..."
+
+// We start a server echoing data on the first stream the client opens,
+// then connect with a client, send the message, and wait for its receipt.
+func main() {
+	err := clientMain()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func clientMain() error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	conn, err := quic.DialAddr(context.Background(), addr, tlsConf, nil)
+	if err != nil {
+		return err
+	}
+
+	stream, err := conn.OpenStreamSync(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Client: Sending '%s'\n", message)
+	_, err = stream.Write([]byte(message))
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, len(message))
+	_, err = io.ReadFull(stream, buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Client: Got '%s'\n", buf)
+
+	return nil
+}
